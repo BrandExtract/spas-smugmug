e8c2b2ef3e008cc7edf4a4ccdc175e6c757e2694
Donovan Buck <donovan@donovan.bz>
2013-05-20 22:20:45 -0500

    Add some error handling

--- api.js
+++ api.js
@@ -14,23 +14,31 @@ exports["custom"] = {
 		params.url = url + "smugmug.albums.get";
 		
 		spashttp.request(params, credentials, function( err, albums ) {
-			
-			var n = albums.Albums.length;
-			
-			_.each(albums.Albums, function( obj, key) {
+			var result, error;
+			if (!err) {
 				
-				params.url = url + "smugmug.images.get" + "&Heavy=true&AlbumID=" + obj.id + "&AlbumKey=" + obj.Key + "&APIKey="+params.APIKey;
+				var n = albums && albums.Albums ? albums.Albums.length : 0;
 				
-				spashttp.request(params, credentials, function( err, photos ) {
-					n = n - 1;
-					albums.Albums[key].Images = photos.Album.Images;
-					albums.size += photos.size;
-					if (n === 0) {
-						cb( null, albums );
-					}
+				_.each(albums.Albums, function( obj, key) {
+					
+					params.url = url + "smugmug.images.get" + "&Heavy=true&AlbumID=" + obj.id + "&AlbumKey=" + obj.Key + "&APIKey="+params.APIKey;
+					
+					spashttp.request(params, credentials, function( err, photos ) {
+						n = n - 1;
+						if (photos.Album) {
+							albums.Albums[key].Images = photos.Album.Images;
+							albums.size += photos.size;
+						}
+						if (n === 0) {
+							cb( null, albums );
+						}
+					});
 				});
-			});
+			} else {
+				cb(error, {} );	
+			}
 		});	
-	},
+		
+	}
 	
 }
